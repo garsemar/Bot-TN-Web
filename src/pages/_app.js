c950b5b04d63cea131fc7d612dcd72bf8e00a8e1
anarve01 <anarve01@gmail.com>
2023-04-14 15:53:00 +0200

    calendar

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,8 +1,10 @@
 import '../styles/style.css'
 import initAuth from '../../initAuth' // the module you created above
 
+
 initAuth()
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
+
